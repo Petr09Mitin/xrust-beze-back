d4022a074f52aec99c675f16f5bb6c8369881118
p.mitin <p.mitin@vkteam.ru>
2025-03-15 14:43:43 +0300

    delivery

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/Petr09Mitin/xrust-beze-back/internal/router"
+	chat_service "github.com/Petr09Mitin/xrust-beze-back/internal/services/chat"
 )
 
 func main() {
-	c := router.NewChat()
+	chatService := chat_service.NewChatService()
+	c := router.NewChat(chatService)
 	err := c.Start()
 	if err != nil {
 		panic(err)
