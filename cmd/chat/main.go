a94357c38a7fc4ff335ce6567bd2d7feaed3ac16
endloc <li.amirova.l@gmail.com>
2025-03-15 15:06:04 +0300

    add basic mongodb interaction

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	client, err := connectToMongo()
+	if err != nil {
+		log.Fatal("Ошибка подключения к MongoDB:", err)
+	}
+	defer client.Disconnect(context.Background())
+
+	err = insertMessage(client, "Привет, MongoDB!")
+	if err != nil {
+		log.Fatal("Ошибка вставки сообщения:", err)
+	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// mongoURI := "mongodb://root:root@mongo_db:27017/admin"
+	mongoURI := "mongodb://admin:admin@mongo_db:27017/xrust_beze?authSource=admin"
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Подключено к MongoDB")
+	return client, nil
+}
+
+func insertMessage(client *mongo.Client, message string) error {
+	collection := client.Database("xrust_beze").Collection("chats")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	doc := bson.M{"message": message, "timestamp": time.Now()}
+
+	_, err := collection.InsertOne(ctx, doc)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Сообщение сохранено в MongoDB:", message)
+	return nil
 }
--- dev/mongo/init-db.js
+++ dev/mongo/init-db.js
@@ -1,3 +1,4 @@
+db = db.getSiblingDB("admin"); // Переключаемся на админскую БД
 db.auth("admin", "admin")
 xb_db = db.getSiblingDB('xrust_beze')
 xb_db.createCollection("chats")
