bab6d5c9e909cb5703f6d90e76684539026ec254
p.mitin <p.mitin@vkteam.ru>
2025-04-12 17:44:34 +0300

    add voice duration in msgs

--- internal/models/chat/bson_message.go
+++ internal/models/chat/bson_message.go
@@ -1,31 +1,35 @@
 package chat_models
 
-import "go.mongodb.org/mongo-driver/v2/bson"
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
 
 type BSONMessage struct {
-	MessageID    bson.ObjectID `bson:"_id,omitempty"`
-	ChannelID    string        `bson:"channel_id"`
-	UserID       string        `bson:"user_id"`
-	PeerID       string        `bson:"peer_id"`
-	Payload      string        `bson:"payload"`
-	Structurized string        `bson:"structurized,omitempty"`
-	Voice        string        `bson:"voice,omitempty"`
-	Attachments  []string      `bson:"attachments,omitempty"`
-	CreatedAt    int64         `bson:"created_at"`
-	UpdatedAt    int64         `bson:"updated_at"`
+	MessageID     bson.ObjectID `bson:"_id,omitempty"`
+	ChannelID     string        `bson:"channel_id"`
+	UserID        string        `bson:"user_id"`
+	PeerID        string        `bson:"peer_id"`
+	Payload       string        `bson:"payload"`
+	Structurized  string        `bson:"structurized,omitempty"`
+	Voice         string        `bson:"voice,omitempty"`
+	VoiceDuration int64         `bson:"voice_duration,omitempty"`
+	Attachments   []string      `bson:"attachments,omitempty"`
+	CreatedAt     int64         `bson:"created_at"`
+	UpdatedAt     int64         `bson:"updated_at"`
 }
 
 func (msg *BSONMessage) ToMessage() Message {
 	return Message{
-		MessageID:    msg.MessageID.Hex(),
-		ChannelID:    msg.ChannelID,
-		UserID:       msg.UserID,
-		PeerID:       msg.PeerID,
-		Payload:      msg.Payload,
-		Structurized: msg.Structurized,
-		CreatedAt:    msg.CreatedAt,
-		UpdatedAt:    msg.UpdatedAt,
-		Voice:        msg.Voice,
-		Attachments:  msg.Attachments,
+		MessageID:     msg.MessageID.Hex(),
+		ChannelID:     msg.ChannelID,
+		UserID:        msg.UserID,
+		PeerID:        msg.PeerID,
+		Payload:       msg.Payload,
+		Structurized:  msg.Structurized,
+		CreatedAt:     msg.CreatedAt,
+		UpdatedAt:     msg.UpdatedAt,
+		Voice:         msg.Voice,
+		Attachments:   msg.Attachments,
+		VoiceDuration: msg.VoiceDuration,
 	}
 }
