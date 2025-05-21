295440dc3762d0738e6f8e9c12d49a65b86fd4c7
endloc <li.amirova.l@gmail.com>
2025-05-21 05:03:28 +0300

    fix study_materials GetByTags format

--- internal/repository/study_material/study_material_repository.go
+++ internal/repository/study_material/study_material_repository.go
@@ -3,6 +3,7 @@ package study_material_repo
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	custom_errors "github.com/Petr09Mitin/xrust-beze-back/internal/models/error"
 	study_material_models "github.com/Petr09Mitin/xrust-beze-back/internal/models/study_material"
@@ -50,9 +51,18 @@ func (r *studyMaterialAPIRepository) GetByID(ctx context.Context, id string) (*s
 	return bsonMaterial.ToStudyMaterial(), nil
 }
 
+// func (r *studyMaterialAPIRepository) GetByTags(ctx context.Context, tags []string) ([]*study_material_models.StudyMaterial, error) {
+// 	// filter := bson.M{"tags": tag}
+// 	filter := bson.M{"tags": bson.M{"$in": tags}}
+// 	return r.find(ctx, filter)
+// }
+
 func (r *studyMaterialAPIRepository) GetByTags(ctx context.Context, tags []string) ([]*study_material_models.StudyMaterial, error) {
-	// filter := bson.M{"tags": tag}
-	filter := bson.M{"tags": bson.M{"$in": tags}}
+	var regexes []interface{}
+	for _, tag := range tags {
+		regexes = append(regexes, bson.M{"$regex": fmt.Sprintf("^%s$", tag), "$options": "i"})
+	}
+	filter := bson.M{"tags": bson.M{"$in": regexes}}
 	return r.find(ctx, filter)
 }
 
